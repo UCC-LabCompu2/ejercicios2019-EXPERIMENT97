b81805f4b7a28c8e96f395c5641d156418ee1e25
EXPERIMENT97 <lucasmuguiro@hotmail.com>
2019-05-29 09:30:25 -0300

    Modifico 29_5

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -40,3 +40,31 @@ function convertir(id_elem, valor_elem) {
         }
     }
 }
+function suma() {
+    document.opeMat.sum_total.value = Number (document.opeMat.sum_num1.value) + Number (document.opeMat.sum_num2.value);
+}
+function rest() {
+    document.opeMat.res_total.value = Number (document.opeMat.res_num1.value) - Number (document.opeMat.res_num2.value);
+}
+function mult() {
+    document.opeMat.mul_total.value = Number (document.opeMat.mul_num1.value) * Number (document.opeMat.mul_num2.value);
+}
+function divi() {
+    document.opeMat.mul_total.value = Number (document.opeMat.div_num1.value) / Number (document.opeMat.div_num2.value);
+}
+function conv_Grados_Rad(id_elem, valor) {
+    if(id_elem=="grados"){
+        document.getElementById("radianes").value = valor*Math.PI/180;
+    }
+    if(id_elem=="radianes"){
+        document.getElementById("grados").value = valor*180/Math.PI;
+    }
+}
+function nostrar_ocultar(id_elem) {
+    if(id_elem=="ocultarDiv"){
+        document.getElementById("unDiv").style.display = 'none';
+    }
+    if(id_elem=="mostrarDiv"){
+        document.getElementById("unDiv").style.display = 'block';
+    }
+}
