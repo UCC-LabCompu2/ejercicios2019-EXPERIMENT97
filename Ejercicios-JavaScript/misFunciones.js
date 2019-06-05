fa46e80a1784218a8c6ff2b4d3665194e4b4c949
EXPERIMENT97 <lucasmuguiro@hotmail.com>
2019-06-05 10:10:07 -0300

    Ejercicio de Canvas

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -68,3 +68,51 @@ function nostrar_ocultar(id_elem) {
         document.getElementById("unDiv").style.display = 'block';
     }
 }
+function dibujarCircCuad() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "#1854ac"; //Color
+    ctx.beginPath(); //comenzar
+    ctx.rect(0, 0, 30, 40); //Posicion de inicio y medidas
+    ctx.fill(); //rellenar
+    ctx.closePath(); //Levantar el lapiz
+
+    ctx.fillStyle = "#af3679";
+    ctx.beginPath();
+    ctx.arc(canvas.width/2, canvas.height/2, 30, 0, 2*Math.PI);
+    ctx.fill();
+    ctx.closePath();
+}
+function dibujarCuadricula() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "#000";
+
+    ctx.beginPath();
+
+    for(var i=0;i<canvas.width;i+=10){
+        ctx.moveTo(i,0);
+        ctx.lineTo(i, canvas.height);
+    }
+    for(var i=0;i<canvas.height;i+=10){
+        ctx.moveTo(0, i);
+        ctx.lineTo(canvas.width, i);
+    }
+    ctx.stroke();
+    ctx.closePath();
+}
+function dibujarImg() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    var img = new Image();
+    img.src = "images/auto.png";
+
+    img.onload = function(){ //pre-carga la funcion para que se ejecute al primer click
+        ctx.drawImage(img, 50, 50);
+    }
+}
